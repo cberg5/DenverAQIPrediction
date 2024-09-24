4d5d36e7340f22f2ddf4772356c32e2c25659427
cjbergin5 <cjbergin5@gmail.com>
2024-09-24 07:44:35 -0600

    configured for heroku

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from web_app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use Heroku's port if available, otherwise default to 5000 for local dev
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
